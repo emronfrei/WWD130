b355cbef532be9643f4ddb353f85b313c374a78b
emronfrei <emronfrei@byui.edu>
2025-12-05 12:26:07 -0500

    added ajusted font size for learn.html

--- WDD130/git_push.sh
+++ WDD130/git_push.sh
@@ -8,7 +8,7 @@ set -e
 git add .
 
 # Commit with a message
-git commit -m "added CSS for learn.html"
+git commit -m "added ajusted font size for learn.html"
 
 # Push to the current branch on origin
 git push
